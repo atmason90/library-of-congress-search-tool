ae82b40f70766bf7e6123826590271b6db74e283
luccaloopz <luccamartins2012@gmail.com>
2022-04-08 17:43:22 -0700

    created the homepage's JS file

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$("#search-form").on("submit", function(event) {
+    event.preventDefault();
+    var searchInputVal = $("#search-input").val();
+    var selectInputVal = $("#select-input").val();
+    if (!searchInputVal) {
+        alert("You must include a search input.");
+        return;
+    };
+    var queryString = "./searchresults.html?q=" + searchInputVal + "&format" + selectInputVal; 
+    location.replace(queryString);
+})
